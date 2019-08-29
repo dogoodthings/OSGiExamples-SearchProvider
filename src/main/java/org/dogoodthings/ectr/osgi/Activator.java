c3d863b14d2535318f60fc761a5a38ac5152feba
dogoodthings <root666@gmx.de>
2019-08-29 11:37:41 +0200

    initial commit

---
+++ src/main/java/org/dogoodthings/ectr/osgi/Activator.java
@@ -0,0 +1,21 @@
+package org.dogoodthings.ectr.osgi;
+
+import org.osgi.framework.BundleActivator;
+import org.osgi.framework.BundleContext;
+
+/**
+ * Activator to register provided services
+ */
+public class Activator implements BundleActivator
+{
+  public void start(BundleContext context) throws Exception
+  {
+    //TODO
+  }
+
+  @Override
+  public void stop(BundleContext bundleContext) throws Exception
+  {
+    //empty
+  }
+}
